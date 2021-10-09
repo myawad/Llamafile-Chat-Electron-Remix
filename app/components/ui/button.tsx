8d2fb9897afa1139bdc1a9a94ed87b39748f9453
myawad <157302185+myawad@users.noreply.github.com>
2021-10-09 15:52:13 +0000

    Update babel monorepo to v7.12.7

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -6,4 +6,4 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus
