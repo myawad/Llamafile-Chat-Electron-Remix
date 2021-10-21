fba3d745d0d10b33e4d227f1ada3c487fcac67c1
myawad <157302185+myawad@users.noreply.github.com>
2021-10-21 07:51:59 +0000

    Proof of concept

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -1,3 +1,8 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils
+import { cn } from "@/lib/utils"
+
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ classNam
