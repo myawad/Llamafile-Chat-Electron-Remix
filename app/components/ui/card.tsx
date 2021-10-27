43fc3ad1b82d6867fbc607b66ff1e463269ae97a
myawad <157302185+myawad@users.noreply.github.com>
2021-10-27 17:04:01 +0000

    Removed useless code.

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -35,4 +35,10 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-    className={cn("font-semibold leading-none tracking-tight", c
+    className={cn("font-semibold leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+CardTitle.displayName = "CardTitle"
+
+const CardDescription = React.fo
