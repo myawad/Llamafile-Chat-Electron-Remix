0e4e0785a4417f33df855c9e331138d3d2a62157
myawad <157302185+myawad@users.noreply.github.com>
2021-10-29 22:00:20 +0000

    procfile for heroku

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -44,4 +44,8 @@ CardTitle.displayName = "CardTitle"
 const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    
