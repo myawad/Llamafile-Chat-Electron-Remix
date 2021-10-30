dc9bca0048f63b37dfc1537ec7d1dc836c35780a
myawad <157302185+myawad@users.noreply.github.com>
2021-10-30 05:36:29 +0000

    css changed

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -48,4 +48,9 @@ const CardDescription = React.forwardRef<
   <p
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
-    
+    {...props}
+  />
+))
+CardDescription.displayName = "CardDescription"
+
+const CardContent = React.f
