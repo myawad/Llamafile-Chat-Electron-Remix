3af4f8652f4384933a4b952489ca122daefec810
myawad <157302185+myawad@users.noreply.github.com>
2021-10-30 21:14:37 +0000

    update bonuses

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -57,4 +57,6 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+))
+CardContent.displayName = "CardC
