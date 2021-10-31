7148a3d48f6abc894cbe5dacf15a500950fc5c96
myawad <157302185+myawad@users.noreply.github.com>
2021-10-31 13:07:06 +0000

    msm: mdss: modify userspace src format during MSMFB_SET on rotator

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -59,4 +59,9 @@ const CardContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
 ))
-CardContent.displayName = "CardC
+CardContent.displayName = "CardContent"
+
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className,
