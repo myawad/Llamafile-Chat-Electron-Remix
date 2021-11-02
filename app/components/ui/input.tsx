d8b68522c19becb0dabd7589b9419172f0c5a755
myawad <157302185+myawad@users.noreply.github.com>
2021-11-02 15:13:43 +0000

    Maven artifacts for Mental State Interface definition version \t\t\t\t\t\t2.0.3-SNAPSHOT

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -11,4 +11,4 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-  
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shad
