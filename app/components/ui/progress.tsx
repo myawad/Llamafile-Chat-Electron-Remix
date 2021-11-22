80d188f60b078d56e3f00621bfc1a564b283d173
myawad <157302185+myawad@users.noreply.github.com>
2021-11-22 11:02:52 +0000

    change db

--- app/components/ui/progress.tsx
+++ app/components/ui/progress.tsx
@@ -6,4 +6,8 @@ import { cn } from "@/lib/utils"
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...pr
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative
