7bcca2b9c3928535271730a90635343d6c8d7c08
myawad <157302185+myawad@users.noreply.github.com>
2021-11-25 10:07:10 +0000

    some corrections

--- app/components/ui/progress.tsx
+++ app/components/ui/progress.tsx
@@ -17,4 +17,8 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
-      sty
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+))
+Progress.disp
