fe50ec9d03b2e0a36522faa5d6a21963212cd0cc
myawad <157302185+myawad@users.noreply.github.com>
2021-12-02 21:18:30 +0000

    Removed useless files

--- app/components/ui/separator.tsx
+++ app/components/ui/separator.tsx
@@ -3,4 +3,6 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
 
-const Separator = React.forwardR
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeo
