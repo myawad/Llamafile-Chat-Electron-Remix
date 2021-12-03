0c2cdfbaec81df39f0d0dbe93fd1d854c1136dee
myawad <157302185+myawad@users.noreply.github.com>
2021-12-03 15:28:08 +0000

    Updated badge

--- app/components/ui/separator.tsx
+++ app/components/ui/separator.tsx
@@ -17,4 +17,11 @@ const Separator = React.forwardRef<
       orientation={orientation}
       className={cn(
         "shrink-0 bg-border",
-       
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  )
+)
+Separ
