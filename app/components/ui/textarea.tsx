36524981a0dab6115718d98db3fc990e96f588cb
myawad <157302185+myawad@users.noreply.github.com>
2021-12-10 11:26:52 +0000

    Events Update

--- app/components/ui/textarea.tsx
+++ app/components/ui/textarea.tsx
@@ -5,4 +5,7 @@ import { cn } from "@/lib/utils";
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
- 
+  disableOnFormSubmission?: boolean;
+}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProp
