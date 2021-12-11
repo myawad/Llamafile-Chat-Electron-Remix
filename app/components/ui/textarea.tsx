830bc37c521039272d97362c6202b614e4fa321b
myawad <157302185+myawad@users.noreply.github.com>
2021-12-11 15:44:48 +0000

    Update copyright notice.

--- app/components/ui/textarea.tsx
+++ app/components/ui/textarea.tsx
@@ -20,4 +20,4 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[60px] w-fu
+          "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground 
