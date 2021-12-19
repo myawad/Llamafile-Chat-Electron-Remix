98cd83ef954390d875a864137aba1478a78104db
myawad <157302185+myawad@users.noreply.github.com>
2021-12-19 05:54:01 +0000

    Update repositories

--- app/components/ui/tooltip.tsx
+++ app/components/ui/tooltip.tsx
@@ -17,3 +17,4 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
+      "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animat
