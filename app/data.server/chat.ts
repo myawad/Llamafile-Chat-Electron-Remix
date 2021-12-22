2b71fa7ed542650168e1310eb9d97a41fbe319a8
myawad <157302185+myawad@users.noreply.github.com>
2021-12-22 21:30:54 +0000

    Updated the build system

--- app/data.server/chat.ts
+++ app/data.server/chat.ts
@@ -1,2 +1,5 @@
 import { asc, desc, eq, sql } from "drizzle-orm";
-import { db, chat, me
+import { db, chat, message } from "@/db.server";
+
+export async function createChat() {
+  const chatCount = await
