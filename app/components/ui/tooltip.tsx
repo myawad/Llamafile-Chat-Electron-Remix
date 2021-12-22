e955eda499b8742fae51745c15f1bbdd2b0d32da
myawad <157302185+myawad@users.noreply.github.com>
2021-12-22 19:05:54 +0000

    New Test

--- app/components/ui/tooltip.tsx
+++ app/components/ui/tooltip.tsx
@@ -25,4 +25,4 @@ const TooltipContent = React.forwardRef<
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export { Tooltip, TooltipTrigger, Tool
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
---
+++ app/data.server/chat.ts
@@ -0,0 +1,2 @@
+import { asc, desc, eq, sql } from "drizzle-orm";
+import { db, chat, me
