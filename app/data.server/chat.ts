81ffda00b8d0af8e7d457a538c4ccbaf995728b2
myawad <157302185+myawad@users.noreply.github.com>
2021-12-23 12:43:08 +0000

    Add new article

--- app/data.server/chat.ts
+++ app/data.server/chat.ts
@@ -2,4 +2,7 @@ import { asc, desc, eq, sql } from "drizzle-orm";
 import { db, chat, message } from "@/db.server";
 
 export async function createChat() {
-  const chatCount = await
+  const chatCount = await db
+    .select({ count: sql<number>`count(*)` })
+    .from(chat);
+  const count = chatCount[0]?.count || 0
