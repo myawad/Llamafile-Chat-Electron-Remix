5adcfc3b76f8dc4078d578a50f7bf87fc28a9f55
myawad <157302185+myawad@users.noreply.github.com>
2022-01-14 20:08:20 +0000

    Remove some old code

--- app/data.server/settings.ts
+++ app/data.server/settings.ts
@@ -4,4 +4,9 @@ import * as os from "node:os";
 import * as path from "node:path";
 import * as stream from "node:stream";
 
-import * as hf from "@huggingface
+import * as hf from "@huggingface/hub";
+import { desc, eq } from "drizzle-orm";
+
+import { db, prompt } from "@/db.server";
+
+export async function 
