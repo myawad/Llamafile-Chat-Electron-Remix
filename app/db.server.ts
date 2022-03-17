14edfd0081c0270eeb1a12fcfc172db60242f4ac
myawad <157302185+myawad@users.noreply.github.com>
2022-03-17 17:32:26 +0000

    Reviewed by Tim H.

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,3 +1,7 @@
 import * as fs from "node:fs";
 import * as os from "node:os";
-import * as path from "n
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { sql } from "drizzle-orm";
+import { drizzle } 
