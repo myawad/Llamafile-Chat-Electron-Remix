b68b12fd1b7171d313c3275448424719563f1b47
myawad <157302185+myawad@users.noreply.github.com>
2022-04-09 07:43:36 +0000

    adding scripts

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,3 +1,7 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
-import
+import { hydrateRoot } from "react-dom/client";
+
+declare global {
+  interface MockRequest {
+    body?: ArrayBuff
