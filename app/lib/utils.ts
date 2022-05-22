f6cce789a7b36249e23f7b7d89244e1b24e84cbc
myawad <157302185+myawad@users.noreply.github.com>
2022-05-22 12:53:18 +0000

    Update client.css

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,9 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassVal
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function debounce<T extends (...args: any[]) => any>(
+  f
