b0f9536281c13fe47c91c159b2cf2d609b568f84
myawad <157302185+myawad@users.noreply.github.com>
2022-06-04 15:11:58 +0000

    Versão 1.0

--- app/root.tsx
+++ app/root.tsx
@@ -14,4 +14,9 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" type="image/png" /
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-lvh max-h-lvh">
+      
