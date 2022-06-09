b62ee3eabeb6ff3402f7fd714af0d02f767de153
myawad <157302185+myawad@users.noreply.github.com>
2022-06-09 06:58:01 +0000

    bidix

--- app/routes/_playground.chat.($chatId)/server.ts
+++ app/routes/_playground.chat.($chatId)/server.ts
@@ -5,4 +5,8 @@ import {
   HumanMessage,
   SystemMessage,
 } from "@langchain/core/messages";
-import { ChatPromptTemplate } from "@l
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+import { getMessages } from "@/data.server/chat";
+import {
+  getLlamafil
