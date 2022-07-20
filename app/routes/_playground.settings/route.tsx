4d1f263f0f9337eaf1e03978a1558ac17580523d
myawad <157302185+myawad@users.noreply.github.com>
2022-07-20 22:35:47 +0000

    Bump to 0.0.5

--- app/routes/_playground.settings/route.tsx
+++ app/routes/_playground.settings/route.tsx
@@ -152,4 +152,9 @@ export default function Index() {
 
   return (
     <div className="container py-4 space-y-4 flex-1 min-h-0 overflow-auto">
-      <h1 className="text-2xl font-semibold">Settin
+      <h1 className="text-2xl font-semibold">Settings</h1>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Prompt Library</CardTitle>
+       
