d6bc2f37f29693a9794ef71146192d347ff125b1
myawad <157302185+myawad@users.noreply.github.com>
2022-09-30 07:07:21 +0000

    markdown

--- app/routes/_playground.settings/server.ts
+++ app/routes/_playground.settings/server.ts
@@ -1,4 +1,7 @@
 import * as path from "node:path";
 
 import { LlamafileModel } from "@/langchain.server/llamafile-model";
-import { getLlamafileDirectory, get
+import { getLlamafileDirectory, getSettings } from "@/data.server/settings";
+
+export async function recompileLlamafile() {
+  const llamafileDir =
