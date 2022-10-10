17c8c9ca616313a1b45ac67347b35e8c1425764b
myawad <157302185+myawad@users.noreply.github.com>
2022-10-10 08:46:58 +0000

    composer install

--- app/routes/setup/route.tsx
+++ app/routes/setup/route.tsx
@@ -81,3 +81,95 @@ export const action = serverOnly$(
           context.ipcEvent("download-phi2-progress", progress)
         );
         return true;
+      case "select-model":
+        const model = String(formData.get("model"));
+        const settings = await getSettings();
+        settings.activeLLM = model;
+        await writeSettings(settings);
+        return true;
+      default:
+        throw new Error("Invalid intent");
+    }
+  }
+);
+
+export default function Setup() {
+  const { activeLLM, baseLlamafile, llamafileDirectory, llms } =
+    useLoaderData<typeof loader>();
+  const downloadLlamafileFetcher = useFetcher();
+  const selectModelFetcher = useFetcher();
+  const revalidator = useRevalidator();
+
+  const showDownloadBase =
+    !baseLlamafile || (baseLlamafile && !!downloadLlamafileFetcher.data);
+
+  const [downloadLlamafileProgress, setDownloadLlamafileProgress] =
+    React.useState(0);
+  React.useEffect(
+    () =>
+      window.electronAPI.onDownloadBaseLlamafileProgress((progress) => {
+        setDownloadLlamafileProgress(progress);
+      }),
+    []
+  );
+  const [downloadPhi2Progress, setDownloadPhi2Progress] = React.useState(0);
+  React.useEffect(
+    () =>
+      window.electronAPI.onDownloadPhi2Progress((progress) => {
+        setDownloadPhi2Progress(progress);
+      }),
+    []
+  );
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center container py-4">
+      <Card className="max-w-screen-md w-full">
+        <CardHeader>
+          <CardTitle>Setup</CardTitle>
+          <CardDescription>Welcome to Remix LLM!</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          {showDownloadBase && (
+            <>
+              <p>
+                Remix LLM needs a copy of the base llamafile to execute models.
+              </p>
+              <downloadLlamafileFetcher.Form method="POST">
+                <input
+                  type="hidden"
+                  name="intent"
+                  value="download-base-llamafile"
+                />
+                <div className="flex gap-4 items-center">
+                  <Button
+                    disabled={!!downloadLlamafileFetcher.data}
+                    variant={
+                      !downloadLlamafileFetcher.data ? "default" : "secondary"
+                    }
+                  >
+                    {downloadLlamafileFetcher.data === true
+                      ? "Download successful"
+                      : downloadLlamafileFetcher.state !== "idle"
+                      ? "Downloading llamafile..."
+                      : "Download llamafile-0.6.2"}
+                  </Button>
+                  {downloadLlamafileFetcher.state !== "idle" && (
+                    <Progress
+                      value={downloadLlamafileProgress}
+                      className="flex-1"
+                    />
+                  )}
+                </div>
+              </downloadLlamafileFetcher.Form>
+            </>
+          )}
+          {(!activeLLM || !!selectModelFetcher.data) && (
+            <>
+              {showDownloadBase && <Separator />}
+              <p>
+                Remix LLM needs a base llamafile model to execute. We recommend
+                using phi-2 as it's lightweight and runs quickly.
+              </p>
+              <selectModelFetcher.Form method="POST">
+                <input
+                  type="hidden"
