0312208983550d43043273bae03613d823872299
myawad <157302185+myawad@users.noreply.github.com>
2022-11-21 06:20:58 +0000

    Updated Hebrew translation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,8 @@
 import * as os from "node:os";
 import * as path from "node:path";
 
-import t
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./app/db.server.ts",
+  out: "./dri
