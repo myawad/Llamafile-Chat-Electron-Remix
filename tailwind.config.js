59a320c813c0199bdab7a437ca680a13e8f9f1c4
myawad <157302185+myawad@users.noreply.github.com>
2024-03-25 09:12:04 +0000

    Some fixes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,4 +21,7 @@ module.exports = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background)
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primar
