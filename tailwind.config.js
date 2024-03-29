70b35a6af8519ed9b7735c93d1f649fb606f0924
myawad <157302185+myawad@users.noreply.github.com>
2024-03-29 13:54:43 +0000

    new packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,4 +58,6 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-    
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion
